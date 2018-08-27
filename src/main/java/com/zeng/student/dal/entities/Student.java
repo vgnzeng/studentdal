7bb7d1957bb24d07a3c060765d8ed9eb49644e51
Guannian Zeng <vgnzeng@gmail.com>
2018-08-26 17:14:23 -0700

    added constructor to Student

--- src/main/java/com/zeng/student/dal/entities/Student.java
+++ src/main/java/com/zeng/student/dal/entities/Student.java
@@ -1,5 +1,6 @@
 package com.zeng.student.dal.entities;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -7,51 +8,65 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-import org.springframework.boot.autoconfigure.web.ResourceProperties.Strategy;
-
 @Entity
-@Table(name="studenttab")
+@Table(name = "studenttab")
 public class Student {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-	private long id;
-	@Column(name="sname")
+	private Long id;
+	@Column(name = "sname")
 	private String name;
-	@Column(name="scourse")
+	@Column(name = "scourse")
 	private String course;
-	@Column(name="sfee")
+	
+	public Student(String name, String course, Double fee) {
+		super();
+		this.name = name;
+		this.course = course;
+		this.fee = fee;
+	}
+
+	@Column(name = "sfee")
 	private Double fee;
 	
-	public long getId() {
+	
+
+	public Long getId() {
 		return id;
 	}
-	public void setId(long id) {
+
+	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getCourse() {
 		return course;
 	}
+
 	public void setCourse(String course) {
 		this.course = course;
 	}
+
 	public Double getFee() {
 		return fee;
 	}
+
 	public void setFee(Double fee) {
 		this.fee = fee;
 	}
-	
+
 	@Override
 	public String toString() {
 		return "Student [id=" + id + ", name=" + name + ", course=" + course + ", fee=" + fee + "]";
 	}
-	
-	
+
 }
