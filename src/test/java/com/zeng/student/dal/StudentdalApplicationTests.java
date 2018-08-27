93350716d2462edfa0107b0c599577d3464279b2
Guannian Zeng <vgnzeng@gmail.com>
2018-08-26 17:08:21 -0700

    added test code to save Student instance into database

--- src/test/java/com/zeng/student/dal/StudentdalApplicationTests.java
+++ src/test/java/com/zeng/student/dal/StudentdalApplicationTests.java
@@ -2,15 +2,24 @@ package com.zeng.student.dal;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.zeng.student.dal.entities.Student;
+import com.zeng.student.dal.repos.StudentRepository;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class StudentdalApplicationTests {
 
+	@Autowired
+	private StudentRepository studentRepository;
+	
 	@Test
-	public void contextLoads() {
+	public void testCreateStudent() {
+		
+		studentRepository.save(new Student("Guan", "Java Spring Boot", 89d));
 	}
 
 }
